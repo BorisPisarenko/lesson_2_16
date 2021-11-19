950ec670aa37f7f2e970dcde4fbb16476d5fbe3c
BorisPisarenko <vasagrigorev94@gmail.com>
2021-11-19 13:13:47 +0200

    #1-2-3-4-5-6 готово

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,23 +15,7 @@ console.log(userObj);
 
 console.log(userObj.fullName());
 
-/*
- * #3
- *
- * Дана функция defUpperStr('My text'), которая возвращает текст, преобразованный в верхний регистр, т.е:
- * defUpperStr('My text') → 'MY TEXT'.
- *
- * Если функция вызывается без параметра defUpperStr(), она не должна возвращать undefined,
- * в этом случае требуется вернуть строку текста по умолчанию в верхнем регистре, т.е:
- * defUpperStr() → 'DEFAULT TEXT'.
- *
- * При выполнении задачи не используйте оператор if, требуется решение с логическим оператором ||.
- */
 
-/* eslint-disable capitalized-comments */
-// console.log(defUpperStr('My text')); // MY TEXT
-
-// console.log(defUpperStr()); // DEFAULT TEXT
 
 function defUpperStr(str) {
     return (str || 'Default text').toUpperCase();
@@ -40,65 +24,58 @@ function defUpperStr(str) {
 console.log(defUpperStr('My text'));
 console.log(defUpperStr());
 
-/*
- * #4
- *
- * Создайте функцию evenFn(n), которая принимает параметром число – количество итераций цикла,
- * т.е. for 0..n. Функция должна вернуть массив, состоящий только из четных значений, генерируемых в цикле.
- *
- * Причем:
- * 0 не должен попадать в результирующий массив
- * цикл должен работать до n включительно
- * разрешен только оператор for
- *
- * Например:
- * evenFn(10) → [2, 4, 6, 8, 10]
- * evenFn(15) → [2, 4, 6, 8, 10, 12, 14]
- * evenFn(20) → [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
- */
 
-// console.log(evenFn(10)); // [2, 4, 6, 8, 10]
-
-// console.log(evenFn(15)); // [2, 4, 6, 8, 10, 12, 14]
-
-// console.log(evenFn(20)); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 function evenFn(n) {
-    let arr = [];
+    var arr = [];
 
-    for (let i = 1; i <= n; i++) if (i % 2 === 0) arr.push(i);
+    for (var i = 1; i <= n; i++) if (i % 2 === 0) arr.push(i);
 
     return arr;
 }
 
-console.log(evenFn(10)); // [2, 4, 6, 8, 10]
-console.log(evenFn(15)); // [2, 4, 6, 8, 10, 12, 14]
-console.log(evenFn(20)); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-/*
- * #5
- *
- * Создайте функцию weekFn(n), которая принимает номер дня недели, а возвращает его название.
- * Если вводится строка, любое дробное число или число вне диапазона 1..7 – функция должна вернуть null.
- *
- * Например:
- * 1 → 'Понедельник'
- * 2 → 'Вторник'
- * ...
- * 7 → 'Воскресенье'
- *
- * В реализации функции обязательно должны быть использованы операторы switch / case / default.
- */
+console.log(evenFn(10));
+console.log(evenFn(15));
+console.log(evenFn(20));
 
-// console.log(weekFn(1)); // 'Понедельник'
 
-// console.log(weekFn(3)); // 'Среда'
+function weekFn(cond) {
+    var str = '';
 
-// console.log(weekFn(7)); // 'Воскресенье'
+    switch (cond) {
+        case 1:
+            str = 'Понедельник';
+            break;
+        case 2:
+            str = 'Вторник';
+            break;
+        case 3:
+            str = 'Среда';
+            break;
+        case 4:
+            str = 'Четверг';
+            break;
+        case 5:
+            str = 'Пятница';
+            break;
+        case 6:
+            str = 'Суббота';
+            break;
+        case 7:
+            str = 'Воскресенье';
+            break;
+        default:
+            str = null;
+    }
 
-// console.log(weekFn(9)); // null
+    return str;
+}
 
-// console.log(weekFn(1.5)); // null
-
-// console.log(weekFn('2')); // null
+console.log(weekFn(1));
+console.log(weekFn(3));
+console.log(weekFn(7));
+console.log(weekFn(9));
+console.log(weekFn(1.5));
+console.log(weekFn('2'));
 
 /*
  * #6
@@ -152,6 +129,40 @@ console.log(evenFn(20)); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
  * console.log(14, ageClassification(122.01) === null);
  * console.log(15, ageClassification(150) === null);
  */
+function ageClassification(num) {
+    return num > 0
+        ? num > 24
+            ? num > 44
+                ? num > 65
+                    ? num > 75
+                        ? num > 90
+                            ? num > 122
+                                ? null
+                                : 'долгожители'
+                            : 'старческий возраст'
+                        : 'пожилой возраст'
+                    : 'средний возраст'
+                : 'молодой возраст'
+            : 'детский возраст'
+        : null;
+}
+
+
+console.log('    -1 :', ageClassification(-1)); // -1 : null
+console.log('     1 :', ageClassification(1)); // 1 : детский возраст
+console.log('    24 :', ageClassification(24)); // 24 : детский возраст
+console.log(' 24.01 :', ageClassification(24.01)); // 24.01 : молодой возраст
+console.log('    44 :', ageClassification(44)); // 44 : молодой возраст
+console.log(' 44.01 :', ageClassification(44.01)); // 44.01 : средний возраст
+console.log('    65 :', ageClassification(65)); // 65 : средний возраст
+console.log('  65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
+console.log('    75 :', ageClassification(75)); // 75 : пожилой возраст
+console.log(' 75.01 :', ageClassification(75.01)); // 75.01 : старческий возраст
+console.log('    90 :', ageClassification(90)); // 90 : старческий возраст
+console.log(' 90.01 :', ageClassification(90.01)); // 90.01 : долгожители
+console.log('   122 :', ageClassification(122)); // 122 : долгожители
+console.log('122.01 :', ageClassification(122.01)); // 122.01 : null
+console.log('   150 :', ageClassification(150)); // 150 : null
 
 /*
  * #7
